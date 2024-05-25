6256cd1d0027fc6fc12f6baaa10683a21faf62bf
Robert Andrew Moore <ra.moore573@gmail.com>
2024-05-25 03:54:47 -0500

    refactor: Update logging in main.js to include CPU usage and bucket status

--- main.js
+++ main.js
@@ -889,7 +889,8 @@ function displayControllerProgress(controller) {
 }
 
 module.exports.loop = function () {
-    console.log('Tick: ' + Game.time + ' || CPU: ' + Game.cpu.getUsed() + ' || Bucket: ' + Game.cpu.bucket);
+    console.log('Tick: ' + Game.time + ' || Bucket: ' + Game.cpu.bucket);
+    const cpuUsedStart = Game.cpu.getUsed();
     for (let roomName in Game.rooms) {
         let room = Game.rooms[roomName];
         if (Game.rooms[roomName].controller && Game.rooms[roomName].controller.my) {
@@ -1099,4 +1100,6 @@ module.exports.loop = function () {
         }
 
     };
+    const cpuUsedEnd = Game.cpu.getUsed();
+    console.log('CPU Used: ' + (cpuUsedEnd - cpuUsedStart));
 };
