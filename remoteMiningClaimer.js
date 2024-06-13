4165444071bb17dc81f5f4775bb2f9ce501d807a
Robert Andrew Moore <ra.moore573@gmail.com>
2024-06-13 05:56:44 +0000

    refactor: Update remoteMiningClaimer to sign controller with random Doctor Who quote

--- remoteMiningClaimer.js
+++ remoteMiningClaimer.js
@@ -1,3 +1,29 @@
+const doctorWhoQuotes = [
+    "I am and always will be the optimist. The hoper of far-flung hopes and the dreamer of improbable dreams.",
+    "The universe is big. It's vast and complicated and ridiculous.",
+    "In 900 years of time and space, I've never met anyone who wasn't important.",
+    "You want weapons? We're in a library! Books! The best weapons in the world!",
+    "Some people live more in 20 years than others do in 80. It's not the time that matters, it's the person.",
+    "We're all stories, in the end. Just make it a good one, eh?",
+    "The way I see it, every life is a pile of good things and bad things. The good things don't always soften the bad things, but vice versa, the bad things don't always spoil the good things and make them unimportant.",
+    "You know when grown-ups tell you everything's going to be fine, and you think they're probably lying to make you feel better?",
+    "There's a lot of things you need to get across this universe. Warp drive... wormhole refractors... You know the thing you need most of all? You need a hand to hold.",
+    "I am definitely a madman with a box.",
+
+    // Add more quotes here
+];
+
+function getRandomDoctorWhoQuote() {
+    const randomIndex = Math.floor(Math.random() * doctorWhoQuotes.length);
+    return doctorWhoQuotes[randomIndex];
+}
+
+function signControllerWithQuote(controller) {
+    const quote = getRandomDoctorWhoQuote();
+    controller.sign(quote);
+}
+
+
 Creep.prototype.remoteClaim = function () {
     // If the creep is not in the target room, go there
     if (this.room.name != this.memory.target) {
@@ -9,6 +35,14 @@ Creep.prototype.remoteClaim = function () {
             if (this.reserveController(controller) == ERR_NOT_IN_RANGE) {
                 this.moveTo(controller);
             }
+        }
+    }
+    const controller = this.room.controller;
+    if (this && controller) {
+        if (this.pos.inRangeTo(controller, 1)) {
+            signControllerWithQuote(controller);
+        } else {
+            this.moveTo(controller);
         }
     }
 };
