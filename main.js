764e1cde8faf925908f565405ee8a82de2c64598
Robert Andrew Moore <ra.moore573@gmail.com>
2024-06-13 06:20:36 +0000

    refactor: Update StructureTower run method to repair damaged structures instead of closestDamagedStructure

--- main.js
+++ main.js
@@ -94,9 +94,9 @@ StructureTower.prototype.run = function () {
     });
     // sort by lowest hits
     damagedStructures.sort((a, b) => a.hits - b.hits);
-    let damagedCreep = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
-        filter: (creep) => {
-            return creep.hits < creep.hitsMax;
+    let damagedCreep = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+        filter: (t) => {
+            return t.hits < t.hitsMax;
         }
     });
     if (hostile.length > 0) {
